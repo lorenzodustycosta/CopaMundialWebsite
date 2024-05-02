8b4126c547ae7be4e8242ac573c66eb43f259d6c
lorenzodustycosta <lorenzodusty.costa@gmail.com>
2024-05-02 16:26:25 +0300

    Fix setting.py

--- football_tournament/football_tournament/settings.py
+++ football_tournament/football_tournament/settings.py
@@ -127,6 +127,7 @@ LOCALE_PATHS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+DEBUG = os.environ['DEBUG']
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)    
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
